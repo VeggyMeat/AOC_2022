316341192291e0ff1074324f2ec9d72ac7f5f60b
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-05 18:06:52 +0000

    Day5 Solution

--- Day5_1.py
+++ Day5_1.py
@@ -1,7 +1,27 @@
 import utils
 
-file = open("Day4.txt")
+file = open("Day5.txt")
 text = file.read()
 file.close()
 
 lines = text.split('\n')
+stacks = [["F", "C", "P", "G", "Q", "R"],
+          ["W", "T", "C", "P"],
+          ["B", "H", "P", "M", "C"],
+          ["L", "T", "Q", "S", "M", "P", "R"],
+          ["P", "H", "J", "Z", "V", "G", "N"],
+          ["D", "P", "J"],
+          ["L", "G", "P", "Z", "F", "J", "T", "R"],
+          ["N", "L", "H", "C", "F", "P", "T", "J"],
+          ["G", "V", "Z", "Q", "H", "T", "C", "W"]]
+
+for line in lines:
+    sections = line.split(' ')
+    number = int(sections[1])
+    from_stack = int(sections[3]) - 1
+    to_stack = int(sections[5]) - 1
+    for x in range(number):
+        stacks[to_stack].append(stacks[from_stack].pop(-1))
+
+for stack in stacks:
+    print(stack[-1], end='')
