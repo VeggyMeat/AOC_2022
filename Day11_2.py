158e01b3c3eb2255c7e840167d80b1f480247036
VeggyMeat <MacraeA@stpaulsschool.org.uk>
2022-12-11 13:55:07 +0000

    Day 11 Solution

---
+++ Day11_2.py
@@ -0,0 +1,41 @@
+import utils
+
+file = open("Day11.txt", 'r')
+data = file.read()
+file.close()
+
+paragraphs = utils.paragraphs(data)
+
+items = []
+operations = [("*", 13), ("+", 3), ("^", 2), ("+", 5), ("+", 7), ("+", 4), ("*", 19), ("+", 2)]
+tests = [5, 7, 13, 11, 3, 2, 17, 19]
+results = [(1, 6), (5, 3), (0, 6), (5, 7), (2, 0), (4, 7), (1, 3), (2, 4)]
+inspects = [0 for x in range(8)]
+biggest = 2*3*5*7*11*13*17*19
+
+for paragraph in paragraphs:
+    lines = paragraph.split('\n')
+    items.append([int(x) for x in lines[1].split(', ')])
+
+for x in range(10000):
+    for monkey in range(len(paragraphs)):
+        for item in items[monkey]:
+            if operations[monkey][0] == "*":
+                item *= operations[monkey][1]
+            elif operations[monkey][0] == "+":
+                item += operations[monkey][1]
+            else:
+                item *= item
+
+            item %= biggest
+            if item % tests[monkey] == 0:
+                items[results[monkey][0]].append(item)
+            else:
+                items[results[monkey][1]].append(item)
+            inspects[monkey] += 1
+
+        items[monkey] = []
+
+inspects.sort()
+print(inspects[-1]*inspects[-2])
+
